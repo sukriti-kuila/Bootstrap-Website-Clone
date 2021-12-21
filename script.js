b4e4df3e9aa1a8d82e93b32dba5bd4a09f45dca1
Sukriti Kuila <87015685+sukriti-kuila@users.noreply.github.com>
2021-12-22 00:16:04 +0530

    Added Typing Animation

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-//  map 
 
-        mapboxgl.accessToken = 'pk.eyJ1Ijoic3Vrcml0aS1rdWlsYSIsImEiOiJja3g5dzN6ZWUwdWZpMzFxYzU0aDF6ZzZoIn0.RbqFAInhDNLlYTVA2ayzLw';
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [88.3639, 22.5726],
-            zoom: 18
-        });
+let i = 0;
+let text = "Full Stack Developer";
+function typewriter() 
+{
+    
+    if (i < text.length) 
+    {
+        document.getElementById("txt").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typewriter, 100);
+    }
+}
+typewriter();
+
+
+//  map 
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3Vrcml0aS1rdWlsYSIsImEiOiJja3g5dzN6ZWUwdWZpMzFxYzU0aDF6ZzZoIn0.RbqFAInhDNLlYTVA2ayzLw';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [88.3639, 22.5726],
+    zoom: 18
+});
+
